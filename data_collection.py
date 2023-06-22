77a6877035d7ff6b1b8f4e44f1de6dbe19e07043
Phillip Long <phillipnlong@gmail.com>
2023-06-21 20:57:57 -0400

    added data_collection

---
+++ data_collection.py
@@ -0,0 +1,10 @@
+# README
+# Phillip Long
+# June 21, 2023
+
+# Collects audio data (.mp3 files) that will be used to train neural networks that determine the
+# tempo, key, and sections of a given song.
+
+
+# IMPORTS
+import librosa
